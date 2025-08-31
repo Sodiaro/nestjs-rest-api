13d320eda77b745932b07e55aef65d1829c12a4c
Sodiaro <semiu.sodiq123@gmail.com>
2025-08-31 10:38:50 +0100

    feat(user): hash passwords before insert using bcrypt

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -6,8 +6,10 @@ import {
   OneToMany,
   ManyToMany,
   JoinTable,
+  BeforeInsert,
 } from 'typeorm';
 import { Property } from './property.entity';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class User {
@@ -29,10 +31,19 @@ export class User {
   @CreateDateColumn()
   createdAt: Date;
 
+  @Column({ nullable: true })
+  password: string;
+
   @OneToMany(() => Property, (property) => property.user)
   properties: Property[];
 
   @ManyToMany(() => Property, (property) => property.likedBy)
   @JoinTable({ name: 'user_liked_properties' })
   likedProperties: Property[];
+
+  @BeforeInsert()
+  async hashPassword() {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  }
 }
