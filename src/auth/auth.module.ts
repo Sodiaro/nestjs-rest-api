ce38a6de2b4b5643c4616446976271b67afa31b9
Sodiaro <semiu.sodiq123@gmail.com>
2025-09-08 19:04:27 +0100

    feat(auth): enable global authentication guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,18 +9,35 @@ import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import refreshJwtConfig  from './config/refresh-jwt.config'
+import refreshJwtConfig from './config/refresh-jwt.config';
 import { RefreshJwtStrategy } from './config/refresh.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+import { RolesGuard } from './guards/roles/roles.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync(jwtConfig.asProvider()),
     ConfigModule.forFeature(jwtConfig),
-      ConfigModule.forFeature(refreshJwtConfig),
+    ConfigModule.forFeature(refreshJwtConfig),
   ],
 
   controllers: [AuthController],
-  providers: [AuthService, UserService, LocalStrategy, JwtStrategy, RefreshJwtStrategy,],
+  providers: [
+    AuthService,
+    UserService,
+    LocalStrategy,
+    JwtStrategy,
+    RefreshJwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard, //@UseGuards(JwtAuthGuard) applied on all API endpoints
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AuthModule {}
